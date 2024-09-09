6f6989d158574c91d3af221a6f589f97a82d6711
Ana Luisa Fuchs <150152111+nalufuchs@users.noreply.github.com>
2024-09-09 19:43:33 -0300

    Update indentation

--- intersection_top100_and_profitable_products.sql
+++ intersection_top100_and_profitable_products.sql
@@ -1,2 +1,11 @@
 CREATE TABLE intersec_top_100_profitable_products as
-SELECT top_100_products_sold.product_name, profitable_products.total_profit, profitable_products.total_sales, profitable_products.total_cost, profitable_products.profit_margin_percentage FROM top_100_products_sold INNER JOIN profitable_products ON top_100_products_sold.product_name = profitable_products.product_name 
+SELECT
+  top_100_products_sold.product_name,
+  profitable_products.total_profit,
+  profitable_products.total_sales,
+  profitable_products.total_cost, 
+  profitable_products.profit_margin_percentage
+FROM
+  top_100_products_sold 
+  INNER JOIN profitable_products 
+    ON top_100_products_sold.product_name = profitable_products.product_name 
